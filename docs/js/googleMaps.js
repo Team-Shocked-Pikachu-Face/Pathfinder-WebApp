a38f05106648456362c9af5f130599931ca84a7d
Andrew Nguyen <andrewnguyen133@g.ucla.edu>
2020-10-30 12:51:25 -0700

    Created google maps js base function that retrieves the values in the text field

---
+++ docs/js/googleMaps.js
@@ -0,0 +1,6 @@
+let getDirections = () => {
+	fromLocation = document.getElementById("from").value;
+	toLocation = document.getElementById("to").value;
+	console.log(fromLocation);
+	console.log(toLocation);
+}
