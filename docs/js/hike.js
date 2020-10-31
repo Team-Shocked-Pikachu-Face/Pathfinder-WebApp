a6ba1d9b8ba38fbadf9fabc9740faaab4513b7d0
Travis Moret <tmoret058@gmail.com>
2020-10-30 21:27:42 -0500

    Added google maps API access and Geolocation function to store lat/lon of user search input

--- docs/js/hike.js
+++ docs/js/hike.js
@@ -5,3 +5,39 @@ document.getElementById("Hike_Search_Bar")
         document.getElementById("Search_Button").click();
     }
 });
+
+
+var latitude; 
+var longitude; 
+
+function geolocate()
+{
+   console.log("test"); 
+
+   var geocoder; 
+
+   geocoder = new google.maps.Geocoder();
+
+   var address = document.getElementById("Hike_Search_Bar").value;
+
+   console.log(address);
+
+   geocoder.geocode({'address': address}, function(results, status) 
+   {  
+      console.log("testing function"); 
+      
+      if(status == 'OK') 
+      {
+         console.log("good status");
+         latitude = results[0].geometry.location.lat();
+         longitude = results[0].geometry.location.lng();
+         console.log(latitude); 
+         console.log(longitude); 
+      } 
+      else 
+      {
+         console.log("bad status");
+         alert("Geocode was not successful for the following reason: " + status);
+      } 
+   });
+}
