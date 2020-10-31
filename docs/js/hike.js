0ec18a905d34bbdc9da3c6384f1a672ba985354b
Travis Moret <tmoret058@gmail.com>
2020-10-31 03:36:43 -0500

    Fixed geolocate function and added comments

--- docs/js/hike.js
+++ docs/js/hike.js
@@ -1,67 +1,78 @@
+// Allow the user to search by pressing enter rather than by using the search button
 document.getElementById("Hike_Search_Bar")
-    .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (KeyboardEvent.code === 13) {
-        document.getElementById("Search_Button").click();
-    }
+   .addEventListener("keyup", function(event) {
+   event.preventDefault();
+   
+   // If the enter key is hit, click the search button
+   if (KeyboardEvent.code === 13) {
+      document.getElementById("Search_Button").click();
+   }
 });
 
-
+// initialize variables to store the user location
 var latitude; 
 var longitude; 
 
-
+/*Summary: geolocate uses the address the user typed into the search bar and sends it to Google's geocoder. The Geocoder returns the latitude and longitude of the address entered. */
 function geolocate()
 {
-   console.log("test"); 
-
+   // Create a geocoder
    var geocoder; 
 
    geocoder = new google.maps.Geocoder();
 
+   // Retrieve the address entered on the search bar
    var address = document.getElementById("Hike_Search_Bar").value;
 
-   console.log(address);
-
+   // Geocode the address
    geocoder.geocode({'address': address}, function(results, status) 
-   {  
-      console.log("testing function"); 
-      
+   {   
+      // If the address exists, get the latitude and longitude
       if(status == 'OK') 
       {
-         console.log("good status");
          latitude = results[0].geometry.location.lat();
          longitude = results[0].geometry.location.lng();
+         
+         // Print to the console for testing
          console.log(latitude); 
          console.log(longitude); 
       } 
+      // Provide an error id the address does not exist
       else 
       {
-         console.log("bad status");
          alert("Geocode was not successful for the following reason: " + status);
       } 
    });
 }
 
-
+/*Summary: The autoComplete function instantiates when the page loads and autocompletes the address the user may be typing. It provides the user with options to autocomplete the entry */
 function autoComplete() 
 {
+   // Get the address the user is typing in the search bar
    var address = (document.getElementById('Hike_Search_Bar'));
    
+   // Autocomplete the address
    var autocomplete = new google.maps.places.Autocomplete(address);
 
+   // Set the type of entries to search for (cities only, etc.)
    autocomplete.setTypes(['geocode']);
 
+   // Continue to check for changes in user input
    google.maps.event.addListener(autocomplete,'place_changed',function() {
+      // Autocomplete a suggestion for a place
       var place = autocomplete.getPlace();
 
+      // If the user enters a place that doesn't exist, return. 
       if (!place.geometry) 
       {
+         window.alert("No details available for input: '" + place.name + "'");
          return;
       }
 
+   // instantiate an array to store address suggestions
    var address = '';
 
+   // Show the autocomplete result options to the user via the array
    if (place.address_components) 
    {
       address = [
@@ -73,4 +84,5 @@ function autoComplete()
    });
 }
 
+// start the autocomplete functionality when the page loads
 google.maps.event.addDomListener(window, 'load', autoComplete);
