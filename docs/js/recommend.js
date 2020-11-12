573b513b3b7655a3846c2dd8f25d93dcf7796997
adew2 <53539900+adew2@users.noreply.github.com>
2020-11-12 02:00:28 -0600

    added a trail class to recreate old trail with new methods, also got weather from OWM API

--- docs/js/recommend.js
+++ docs/js/recommend.js
@@ -2,7 +2,7 @@
 // console.log(trail);
 
 // this is test trail info to use while making recommend page, normally have selected trail from hike.html being saved in sessionStorage(clicking on trail's image)
-var trail = {
+var storedTrail = {
     difficulty: "MEDIUM",
     elevationGain: 2503,
     highestElevation: 10656,
@@ -13,4 +13,42 @@ var trail = {
     name: "Bear Lake TH to Fern Lake TH",
     photo: "https://cdn2.apstatic.com/photos/hike/7000772_medium_1554159640.jpg"
 }
+
+
+class Trail {
+    constructor(passedTrail) {
+    this.length = passedTrail.length;
+    this.latitude = passedTrail.latitude;
+    this.longitude = passedTrail.longitude;
+    this.highestElevation = passedTrail.highestElevation;
+    this.elevationGain = passedTrail.elevationGain;
+    this.difficulty = passedTrail.difficulty;
+    this.photo = passedTrail.photo;
+    this.name = passedTrail.name;
+    this.location = passedTrail.location;
+    }
+    async getWeather () {
+       const weatherAPIURL = `https://api.openweathermap.org/data/2.5/weather?lat=${this.latitude}&lon=${this.longitude}&appid=e61074558f08136b4d111a532200ab94&units=imperial`
+       return fetch(weatherAPIURL)
+            .then(response => response.json())
+            .then((data) => {
+                this.weather = data
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+    getRecommendations() {
+        console.log("getting Recommendation");
+    }
+}
+
+var focusedTrail = new Trail(storedTrail)
+var promises = [];
+promises.push(focusedTrail.getWeather());
+Promise.all(promises).then(() => {
+    console.log(promises);
+    console.log(focusedTrail.weather);
+    focusedTrail.getRecommendations();
+});
 
