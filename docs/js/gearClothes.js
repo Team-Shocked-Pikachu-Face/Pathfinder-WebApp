419b7c0b04b226abbd73a8e1c0e34cc8f33198f5
Travis Moret <tmoret058@gmail.com>
2020-11-27 20:12:10 -0600

    Added clothing descriptions for some clothing items on recommendation/ gear page

--- docs/js/gearClothes.js
+++ docs/js/gearClothes.js
@@ -100,50 +100,68 @@ Weather codes for the below object creations can be found at: https://openweathe
 
 /* Warm Clothing Objects*/
 //head wear
-const warmHead1 = new Clothing('./clothes/warmHead1.jpg', '', [60, 120], 'head', [800]);
+let warmHead1Description = "Ballcaps are cool in warm weather and keep the sun off of your face"; 
+const warmHead1 = new Clothing('./clothes/warmHead1.jpg', warmHead1Description, [60, 120], 'head', [800]);
 gearClothesHolder.storeClothes(warmHead1);
-const warmHead2 = new Clothing('./clothes/warmHead2.jpg', '', [60, 120], 'head', [800]);
+let warmHead2Description = "Wide brimmed caps protect the neck and head from the sun";
+const warmHead2 = new Clothing('./clothes/warmHead2.jpg', warmHead2Description, [60, 120], 'head', [800]);
 gearClothesHolder.storeClothes(warmHead2);
-const warmHead3 = new Clothing('./clothes/warmHead3.jpg', '', [60, 120], 'head', [300]);
+let warmHead3Description = "Boony hats offer great head an neck protection from the sun ";
+const warmHead3 = new Clothing('./clothes/warmHead3.jpg', warmHead3Description, [60, 120], 'head', [300]);
 gearClothesHolder.storeClothes(warmHead3);
 //jackets
-const warmJacket1 = new Clothing('./clothes/warmJacket1.jpg', '', [60,70], 'jacket', [200, 300, 500, 600, 700, 800, 802]);
+let warmJacket1Description = "Light pullovers can provide a light extra layer in warm or slightly cool conditions";
+const warmJacket1 = new Clothing('./clothes/warmJacket1.jpg', warmJacket1Description, [60,70], 'jacket', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(warmJacket1);
-const warmJacket2 = new Clothing('./clothes/warmJacket2.jpg', '', [60,70], 'jacket', [200, 300, 500, 600, 700, 800, 802]);
+let warmJacket2Description = "Sweatshirts can provide a light extra layer in warm or slightly cool conditions";
+const warmJacket2 = new Clothing('./clothes/warmJacket2.jpg', warmJacket2Description, [60,70], 'jacket', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(warmJacket2);
 //shirts
-const warmShirt1 = new Clothing('./clothes/warmShirt1.jpg', '', [70,120], 'shirt', [200, 300, 500, 600, 700, 800, 802]);
+let warmShirt1Description = "Short sleeved t-shirts are as great base layer for warm or slighly cool conditions";
+const warmShirt1 = new Clothing('./clothes/warmShirt1.jpg', warmShirt1Description, [70,120], 'shirt', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(warmShirt1);
-const warmShirt2 = new Clothing('./clothes/warmShirt2.jpg', '', [70,120], 'shirt', [200, 300, 500, 600, 700, 800, 802]);
+let warmShirt2Description = "Long sleeved button up shirts provide cool sun protection on the arms and neck"; 
+const warmShirt2 = new Clothing('./clothes/warmShirt2.jpg', warmShirt2Description, [70,120], 'shirt', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(warmShirt2);
 //pants/shorts
-const warmPants1 = new Clothing('./clothes/warmPants1.jpg', '', [60, 120], 'pant', [200, 300, 500, 600, 700, 800, 802]);
+let warmPants1Description = "Long legged cargo pants can protect from the sun as well as poison ivy and ticks. They may have the added benefit of many pockets as well";
+const warmPants1 = new Clothing('./clothes/warmPants1.jpg', warmPants1Description, [60, 120], 'pant', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(warmPants1);
-const warmPants2 = new Clothing('./clothes/warmPants2.jpg', '', [60, 120], 'pant', [200, 300, 500, 600, 700, 800, 802]);
+let warmPants2Description = "Long pants can help keep the legs warm and protect from poinonous plants or ticks"; 
+const warmPants2 = new Clothing('./clothes/warmPants2.jpg', warmPants2Description, [60, 120], 'pant', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(warmPants2);
-const warmShorts1 = new Clothing('./clothes/warmShorts1.jpg', 'cargo shorts', [70, 120], 'pant', [200, 300, 500, 600, 700, 800, 802]);
+let warmShorts1Description = "Cargo shorts are a lightweight alternative to long legged pants that can be cooler in warm weather"; 
+const warmShorts1 = new Clothing('./clothes/warmShorts1.jpg', warmShorts1Description, [70, 120], 'pant', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(warmShorts1);
 
 /* Cold Clothing Objects*/
 //head wear
-const coldHead1 = new Clothing('./clothes/coldHead1.jpg', '', [-20,60], 'head', [200, 300, 500, 600, 700, 800, 802]);
+let coldHead1Description = "Knit caps are very warm in windy or cold conditions"; 
+const coldHead1 = new Clothing('./clothes/coldHead1.jpg', coldHead1Description, [-20,60], 'head', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(coldHead1);
-const coldHead2 = new Clothing('./clothes/coldHead2.jpg', 'Due to the cold weather, one recommendation is to wear an insulated beanie. This will provide maximum warmth.', [-20,60], 'head', [200, 300, 500, 600, 700, 800, 802]);
+let coldHead2Description = "Due to the cold weather, one recommendation is to wear an insulated beanie. This will provide maximum warmth.";
+const coldHead2 = new Clothing('./clothes/coldHead2.jpg', coldHead2Description, [-20,60], 'head', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(coldHead2);
 //jackets
-const coldJacket1 = new Clothing('./clothes/coldJacket1.jpg', '', [45,60], 'jacket', [200, 300, 500, 600, 700, 800, 802]);
+let coldJacket1Description = "Windbreaker Jackets are especially helpful at keeping warm in windy and cold conditions"; 
+const coldJacket1 = new Clothing('./clothes/coldJacket1.jpg', coldJacket1Description, [45,60], 'jacket', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(coldJacket1);
-const coldJacket2 = new Clothing('./clothes/coldJacket2.jpg', '', [45,60], 'jacket', [200, 300, 500, 600, 700, 800, 802]);
+let coldJacket2Description = "Puffy Jackets trap warm air inside them and create a great insulating layer. They are also easy to pack and great to use as a pillow"; 
+const coldJacket2 = new Clothing('./clothes/coldJacket2.jpg', coldJacket2Description, [45,60], 'jacket', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(coldJacket2);
-const coldJacket3 = new Clothing('./clothes/coldJacket3.jpg', '', [45,60], 'jacket', [200, 300, 500, 600, 700, 800, 802]);
+let coldJacket3Description = "Windbreaker Jackets are especially helpful at keeping warm in windy and cold conditions";
+const coldJacket3 = new Clothing('./clothes/coldJacket3.jpg', coldJacket3Description, [45,60], 'jacket', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(coldJacket3);
 //shirts
-const longSleeve1 = new Clothing('./clothes/longSleeve1.jpg', '', [-20,70], 'shirt', [200, 300, 500, 600, 700, 800, 802]);
+let longSleeve1Description = "Longsleeve base layers such as this can be a great extra insulating layer in cold conditions"; 
+const longSleeve1 = new Clothing('./clothes/longSleeve1.jpg', longSleeve1Description, [-20,70], 'shirt', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(longSleeve1);
-const longSleeve2 = new Clothing('./clothes/longSleeve2.jpg', '', [-20,70], 'shirt', [200, 300, 500, 600, 700, 800, 802]);
+let longSleeve2Description = "Longsleeve base layers like this one can make for a great light layer in cool conditions. They also protect the arms and shoulders from the sun"; 
+const longSleeve2 = new Clothing('./clothes/longSleeve2.jpg', longSleeve2Description, [-20,70], 'shirt', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(longSleeve2);
 //pants
-const coldPants2 = new Clothing('./clothes/coldPants2.jpg', 'windproof and waterproof', [45, 60], 'pant', [200, 300, 500, 600, 700, 800, 802]);
+let coldPants2Description = "Long pants can provide warmth to the legs by keeping cold wind off and may be waterproof as well"; 
+const coldPants2 = new Clothing('./clothes/coldPants2.jpg', coldPants2Description, [45, 60], 'pant', [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeClothes(coldPants2);
 
 /* Winter Clothing Objects*/
@@ -172,7 +190,8 @@ gearClothesHolder.storeClothes(hikingBootCold2);
 
 /* Gear Objects */
 //hiking poles
-const hikingPoles = new Gear('./gear/hikingPoles.jpg', '', [-20,130], 700, [0,200], [200, 300, 500, 600, 700, 800, 802]);
+let hikingPoleDescription = "Hiking poles are especially helpful on longer hikes and can reduce the strain on a hikers knees, especially on trails with changes in elevation"; 
+const hikingPoles = new Gear('./gear/hikingPoles.jpg', hikingPoleDescription, [-20,130], 700, [0,200], [200, 300, 500, 600, 700, 800, 802]);
 gearClothesHolder.storeGear(hikingPoles);
 //water bottle
 const waterBottle = new Gear('./gear/waterBottle.jpg', '', [-20,130], 0, [0, 6], [200, 300, 500, 600, 700, 800, 802]);
